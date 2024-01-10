e8e5190c2f7237c451bd2ab7e8eb4e5e1e34a0a8
Ayush Kumar <ayushishan08@gmail.com>
2024-01-10 15:42:05 +0530

    added assets for different service type

--- src/Components/ServiceList.js
+++ src/Components/ServiceList.js
@@ -1,22 +1,25 @@
 // ServiceList.js
 import React from 'react';
 import BookingCard from './BookingCard';
+import Maidpicture from '../Assets/maid.png';
+import cookpicture from '../Assets/cook.png';
+import electricianpicture from '../Assets/electrician.png';
 
 const ServiceList = ({ onServiceSelect }) => {
   const services = [
-    { id: 1, name: 'Maid' },
-    { id: 2, name: 'Cook' },
-    { id: 3, name: 'Electrician' },
+    { id: 1, name: 'Maid', picture: Maidpicture },
+    { id: 2, name: 'Cook', picture: cookpicture},
+    { id: 3, name: 'Electrician', picture: electricianpicture },
     // Add more services as needed
   ];
 
   return (
     <div>
-      <div>
-        <h2> Available Services</h2>
+      <div class="text-center font-semibold tracking-wide text-2xl">
+        <h1> Available Services</h1>
       </div>
-      <div class= "flex flex-row text-center text-white">
-        {services.map((element) => <BookingCard name={element.name}/>)}
+      <div class= "flex flex-row text-center">
+        {services.map((element) => <BookingCard name={element.name} image={element.picture}/>)}
       </div>
     </div>
   );
