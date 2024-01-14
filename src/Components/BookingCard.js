a3aff26205439231a0fde2d6bef65a9a37e1f7df
Ayush Kumar <ayushishan08@gmail.com>
2024-01-14 20:22:27 +0530

    Added date picker in the booking card

--- src/Components/BookingCard.js
+++ src/Components/BookingCard.js
@@ -1,4 +1,7 @@
 import {Link} from 'react-router-dom'
+import {DateTimePicker,LocalizationProvider} from '@mui/x-date-pickers'
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
+
 
 const BookingCard = (props) => {
     return (
@@ -9,14 +12,23 @@ const BookingCard = (props) => {
           <div className="m-3 font-medium tracking-widest">
             {props.name}
           </div>
-            <Link to={"/book/" + props.name}> 
-            {/* TODO : the whole div is clickable we have to correct the CSS */}
+          <div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker 
+                onChange={(date_time,validation) => {console.log("onchange",date_time, validation)}} 
+                onAccept={(a) => {console.log("onaccept", a)}}
+                onClose={() => {console.log("onclose")}}
+                disablePast={true}
+                label="Choose Your Slot"/>
+            </LocalizationProvider>
+          </div>
             <div className="flex justify-center">
-              <div className="m-4 box-border bg-gray-300 p-4 hover:bg-gray-500 w-1/3 cursor-pointer">
-                <button className="tracking-wide">Book Now</button>
+              <div className="m-4 box-border bg-gray-300 p-4 hover:bg-gray-500 w-1/3">
+                <Link to={"/book/" + props.name}> 
+                  <button className="tracking-wide" disabled={false}>Book Now</button>
+                </Link>
               </div>
             </div>
-            </Link>
         </div>
     )
 }
