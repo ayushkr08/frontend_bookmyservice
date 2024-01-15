fe18df22772d46c772eaac64bbca528d782a0d7e
Ayush Kumar <ayushishan08@gmail.com>
2024-01-15 16:06:34 +0530

    added sort by rating feature(high to low)

--- src/Components/SortBar.js
+++ src/Components/SortBar.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsUpDown } from "@fortawesome/free-solid-svg-icons";
-const SortBar = () => {
+const SortBar = (props) => {
   return (
     <div className="box-border w-1/2 m-auto flex flex-row justify-around bg-gray-300 shadow-2xl shadow-gray-500/50">
       <div>SORT BY :</div>
       <div className="space-x-1">
-        <button>Rating</button>
+        <button onClick={props.ratingClickHandler}>Rating</button>
         <FontAwesomeIcon icon={faArrowsUpDown} />
       </div>
       <div className="space-x-1">
--- src/Components/WorkerList.js
+++ src/Components/WorkerList.js
@@ -2,11 +2,22 @@ import { useParams } from "react-router-dom";
 import WorkerCard from "./WorkerCard";
 import { maidData } from "../DummyData/maidData";
 import SortBar from "./SortBar";
+import { useState } from "react";
 const WorkerList = () => {
+  let [localMaidData, setlocalMaidData] = useState(maidData);
+  const ratingClickHandler = (e) => {
+    console.log("localMaiddata", localMaidData);
+    // ALways make a copy of state variable if you are going to change it.
+    let copyOfLocalMaidData = JSON.parse(JSON.stringify(localMaidData));
+    copyOfLocalMaidData.sort((maid1, maid2) => {
+      return maid2.Rating - maid1.Rating;
+    });
+    setlocalMaidData(copyOfLocalMaidData);
+  };
   return (
     <>
-      <SortBar />
-      {maidData.map((singleWorker) => {
+      <SortBar ratingClickHandler={ratingClickHandler} />
+      {localMaidData.map((singleWorker) => {
         return <WorkerCard key={singleWorker.Name} data={singleWorker} />;
       })}
     </>
