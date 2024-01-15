6bd731029d986d6d05e4d06cdee09b441cfe10ed
Ayush Kumar <ayushishan08@gmail.com>
2024-01-15 16:20:36 +0530

    added toggle sort for ratings

--- src/Components/WorkerList.js
+++ src/Components/WorkerList.js
@@ -3,14 +3,25 @@ import WorkerCard from "./WorkerCard";
 import { maidData } from "../DummyData/maidData";
 import SortBar from "./SortBar";
 import { useState } from "react";
+let isRatingSortedHighToLow = null;
 const WorkerList = () => {
   let [localMaidData, setlocalMaidData] = useState(maidData);
   const ratingClickHandler = (e) => {
-    console.log("localMaiddata", localMaidData);
+    if (isRatingSortedHighToLow == null) {
+      isRatingSortedHighToLow = true;
+    } else if (isRatingSortedHighToLow == true) {
+      isRatingSortedHighToLow = false;
+    } else {
+      isRatingSortedHighToLow = true;
+    }
     // ALways make a copy of state variable if you are going to change it.
     let copyOfLocalMaidData = JSON.parse(JSON.stringify(localMaidData));
     copyOfLocalMaidData.sort((maid1, maid2) => {
-      return maid2.Rating - maid1.Rating;
+      if (isRatingSortedHighToLow == true) {
+        return maid2.Rating - maid1.Rating;
+      } else if (isRatingSortedHighToLow == false) {
+        return maid1.Rating - maid2.Rating;
+      }
     });
     setlocalMaidData(copyOfLocalMaidData);
   };
