d392aea6391bec65a973ee8d0e8ef59eaecdfbec
Ayush Kumar <ayushishan08@gmail.com>
2024-01-15 15:42:38 +0530

    added icon for sort bar and its components

--- src/Components/SortBar.js
+++ src/Components/SortBar.js
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsUpDown } from "@fortawesome/free-solid-svg-icons";
 const SortBar = () => {
   return (
     <div className="box-border w-1/2 m-auto flex flex-row justify-around bg-gray-300 shadow-2xl shadow-gray-500/50">
       <div>SORT BY :</div>
-      <div>Rating</div>
-      <div>Price</div>
-      <div>Distance</div>
+      <div className="space-x-1">
+        <button>Rating</button>
+        <FontAwesomeIcon icon={faArrowsUpDown} />
+      </div>
+      <div className="space-x-1">
+        <button>Price</button>
+        <FontAwesomeIcon icon={faArrowsUpDown} />
+      </div>
+      <div className="space-x-1">
+        <button>Distance</button>
+        <FontAwesomeIcon icon={faArrowsUpDown} />
+      </div>
     </div>
   );
 };
