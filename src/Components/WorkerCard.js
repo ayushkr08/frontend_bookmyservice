4132eb856d438f8d68b2635a12cba7310b8d57b3
Ayush Kumar <ayushishan08@gmail.com>
2024-01-22 14:18:25 +0530

    added new type of rating mechanism using material UI

--- src/Components/WorkerCard.js
+++ src/Components/WorkerCard.js
@@ -1,6 +1,7 @@
 import sampleImage from "../DummyData/Assets/sampleWorkerImage.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faIndianRupeeSign } from "@fortawesome/free-solid-svg-icons";
+import Rating from "@mui/material/Rating";
 const WorkerCard = (props) => {
   return (
     <div className="justify-between box-border flex flex-row w-1/3 shadow-2xl shadow-gray-500/50 rounded-lg m-auto mt-8">
@@ -9,9 +10,17 @@ const WorkerCard = (props) => {
         <div>{"Name : " + props.data.Name}</div>
         <div>{"Experience : " + props.data.Experience + "+ yrs"}</div>
         <div>{props.data.Distance + " kms away"}</div>
-        <div>
+        {/* <div>
           {props.data.Rating + " "}
           <FontAwesomeIcon icon={faStar} />
+        </div> */}
+        <div>
+          <Rating
+            name="half-rating"
+            defaultValue={props.data.Rating}
+            precision={0.1}
+            readOnly
+          />
         </div>
         <div>
           <FontAwesomeIcon icon={faIndianRupeeSign} />
--- src/Components/WorkerList.js
+++ src/Components/WorkerList.js
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import WorkerCard from "./WorkerCard";
 import { maidData } from "../DummyData/maidData";
 import SortBar from "./SortBar";
